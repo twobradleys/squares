b9a469a5f73b7e832fcaad93ba7a0aa8724ec643
Bradley Buda <bradleybuda@gmail.com>
2016-12-20 17:07:59 -0800

    Bunch of picking logic

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import {
   Text,
   View,
   Image,
-  TouchableHighlight
+  TouchableHighlight,
+  Button,
+  AlertIOS
 } from 'react-native';
 
 class Cell extends React.Component {
@@ -13,13 +15,14 @@ class Cell extends React.Component {
     super(props);
     this.rowIndex = props.rowIndex;
     this.colIndex = props.colIndex;
+    this.onPickCell = props.onPickCell;
     this.state = {owner: null};
   }
 
   _onPressButton = () => {
-    console.log("You tapped the button!");
     // TODO assign to current picker, or unassign / replace
-    this.setState({owner: 'BB'});
+    let owner = this.onPickCell(this.rowIndex, this.colIndex);
+    this.setState({owner: owner});
   };
 
   render () {
@@ -34,6 +37,11 @@ class Cell extends React.Component {
 }
 class Grid extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.onPickCell = props.onPickCell;
+  }
+
   render () {
     // TODO shuffle after all squares picked
     let columnHeaders = [7,5,3,9,0,1,4,6,2,8];
@@ -42,6 +50,8 @@ class Grid extends React.Component {
     let dims = [...Array(10)];
 
     let cellStyle = {width:25,height:25,borderWidth:1,borderColor:'black'};
+
+    let onPickCell = this.onPickCell;
 
     return (
       <View style={{flexDirection:'column',borderWidth:1,borderColor:'black'}}>
@@ -53,9 +63,9 @@ class Grid extends React.Component {
         </View>
         {dims.map(function(_, i){
           return <View key={i} style={{flexDirection: 'row'}}>
-            <View key="rowHeader" style={cellStyle}><Text>{rowHeaders[i]}</Text></View>
+            <View key="rowHeaders" style={cellStyle}><Text>{rowHeaders[i]}</Text></View>
             {dims.map(function(_, j){
-              return <Cell key={j} rowIndex={i} colIndex={j} />;
+              return <Cell key={j} rowIndex={i} colIndex={j} onPickCell={onPickCell} />;
             })}
           </View>
         })}
@@ -64,11 +74,59 @@ class Grid extends React.Component {
   }
 }
 
+class PlayerList extends React.Component {
+  render () {
+    return (
+      <View style={{flexDirection:'row'}}>
+        {this.props.players.map(function(player,i){
+          return <View style={{padding: 5}} key={i}><Text>{player.name} ${player.picks.length * 0.25}</Text></View>;
+        })}
+      </View>
+    )
+  }
+}
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {players: [], currentPicker: null};
+  }
+
+  addPlayer = (name) => {
+    let s = this.state;
+    s.players.push({name: name, picks: []});
+    s.currentPicker = name;
+    this.setState(s);
+  }
+
+  promptToAddPlayer = () => {
+    AlertIOS.prompt(
+      'Add Player',
+      'Enter the new player\'s initials',
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: this.addPlayer}
+      ],
+      'plain-text'
+    );
+  }
+
+  onPickCell = (row, col) => {
+    let s = this.state;
+    let player = s.players.find((p) => p.name == this.state.currentPicker);
+    player.picks.push([row, col]);
+    this.setState(s);
+
+    return this.state.currentPicker; // to render; TODO this should be state-based
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Grid />
+        <Grid onPickCell={this.onPickCell} />
+        <PlayerList players={this.state.players} />
+        <Text>{this.state.currentPicker} is picking</Text>
+        <Button title="Add Player" onPress={this.promptToAddPlayer} />
       </View>
     );
   }
