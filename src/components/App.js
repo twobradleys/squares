9a17411cd224d215d5f30d62a3476979ef4663ae
Bradley Buda <bradleybuda@gmail.com>
2016-12-22 16:50:30 -0800

    Remove Todo stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
 
 import AddPlayer from '../containers/AddPlayer'
 import EnrolledPlayerList from '../containers/EnrolledPlayerList'
 
 const App = () => (
   <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-
-
     <EnrolledPlayerList />
     <AddPlayer />
   </div>
