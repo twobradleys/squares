2e57b96dbbe292403f7673fa0861322997181d1e
Bradley Buda <bradleybuda@gmail.com>
2016-12-27 20:09:48 -0800

    Switch to react-actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,33 +1,13 @@
+import { createAction } from 'redux-actions'
+
 // TODO use Immutable Maps for actions? Framework might not like that
 
 import Immutable from 'immutable'
 
-let nextPlayerId = 0;
+let nextPlayerId = 0
 
-export const addPlayer = (name) => ({
-  type: 'ADD_PLAYER',
-  id: nextPlayerId++,
-  name
-})
-
-export const selectPlayer = (id) => ({
-  type: 'SELECT_PLAYER',
-  id
-})
-
-export const clickGridSquare = ({i, j}) => ({
-  type: 'CLICK_GRID_SQUARE',
-  i,
-  j
-})
-
-export const makeQuickPicks = (count) => ({
-  type: 'MAKE_QUICK_PICKS',
-  count: count,
-  seed: Math.random()
-})
-
-export const lockEntries = () => ({
-  type: 'LOCK_ENTRIES',
-  digitSeeds: Immutable.Map({home: Math.random(), away: Math.random()})
-})
+export const addPlayer = createAction('ADD_PLAYER', name => ({id: nextPlayerId++, name}))
+export const selectPlayer = createAction('SELECT_PLAYER', id => ({id}))
+export const clickGridSquare = createAction('CLICK_GRID_SQUARE') // Payload is a map of { i, j }
+export const makeQuickPicks = createAction('MAKE_QUICK_PICKS', count => ({count, seed: Math.random()}))
+export const lockEntries = createAction('LOCK_ENTRIES', () => ({digitSeeds: Immutable.Map({home: Math.random(), away: Math.random()})}))
--- src/reducers/grid.js
+++ src/reducers/grid.js
@@ -1,6 +1,7 @@
 import Immutable from 'immutable'
 import shuffleSeed from 'shuffle-seed'
 import { findUnclaimedSquares } from '../selectors'
+import { handleActions } from 'redux-actions'
 
 const dims = Immutable.Range(0, 10).toList()
 const noEntries = dims.map(() => dims.map(() => null))
@@ -14,53 +15,45 @@ const initialState = Immutable.Map({
   digits: Immutable.Map({home: noDigits, away: noDigits})
 })
 
-const grid = (state = initialState, action) => {
-  switch (action.type) {
+const grid = handleActions({
+  ADD_PLAYER: (state, action) => {
+    return state.update('players', players => players.push(Immutable.Map(action.payload)))
+                .set('pickingPlayerId', action.payload.id)
+  },
 
-    case 'ADD_PLAYER': {
-      return state
-        .update('players', players => players.push(Immutable.Map({id: action.id, name: action.name})))
-        .set('pickingPlayerId', action.id)
+  SELECT_PLAYER: (state, action) => {
+    // Acts as a toggle; either set a new picking player, or "turn off" the current one
+    const pickingPlayerId = state.get('pickingPlayerId')
+    if (pickingPlayerId === action.payload.id) {
+      return state.set('pickingPlayerId', null)
+    } else {
+      return state.set('pickingPlayerId', action.payload.id)
     }
+  },
 
-    case 'SELECT_PLAYER': {
-      // Acts as a toggle; either set a new picking player, or "turn off" the current one
-      const pickingPlayerId = state.get('pickingPlayerId')
-      if (pickingPlayerId === action.id) {
-        return state.set('pickingPlayerId', null)
-      } else {
-        return state.set('pickingPlayerId', action.id)
-      }
-    }
-
-    case 'CLICK_GRID_SQUARE': {
-      if (state.getIn(['locked']) === false) {
-        return state.setIn(['entries', action.i, action.j], state.get('pickingPlayerId'))
-      } else {
-        return state;
-      }
-    }
-
-    case 'MAKE_QUICK_PICKS': {
-      const pickingPlayerId = state.get('pickingPlayerId')
-      const unclaimedSquares = findUnclaimedSquares(state.get('entries'))
-      const quickPickSquares = Immutable.fromJS(shuffleSeed.shuffle(unclaimedSquares.toJS(), action.seed)).take(action.count)
-      return state
-        .update('entries', entries =>
-          quickPickSquares.reduce((e, square) =>
-            e.setIn([square.get('i'), square.get('j')], pickingPlayerId),
-                               entries))
-    }
-
-    case 'LOCK_ENTRIES': {
-      const digits = action.digitSeeds.map((team, seed) => Immutable.fromJS(shuffleSeed.shuffle(Immutable.Range(0,10).toJS(), seed)))
-      return state.set('digits', digits).set('pickingPlayerId', null).set('locked', true)
-    }
-
-    default: {
+  CLICK_GRID_SQUARE: (state, action) => {
+    if (state.get('locked') === false) {
+      return state.setIn(['entries', action.payload.i, action.payload.j], state.get('pickingPlayerId'))
+    } else {
       return state
     }
+  },
+
+  MAKE_QUICK_PICKS: (state, action) => {
+    const pickingPlayerId = state.get('pickingPlayerId')
+    const unclaimedSquares = findUnclaimedSquares(state.get('entries'))
+    const quickPickSquares = Immutable.fromJS(shuffleSeed.shuffle(unclaimedSquares.toJS(), action.payload.seed)).take(action.payload.count)
+    return state
+      .update('entries', entries =>
+        quickPickSquares.reduce((e, square) =>
+          e.setIn([square.get('i'), square.get('j')], pickingPlayerId),
+                                entries))
+  },
+
+  'LOCK_ENTRIES': (state, action) => {
+    const digits = action.payload.digitSeeds.map((team, seed) => Immutable.fromJS(shuffleSeed.shuffle(Immutable.Range(0,10).toJS(), seed)))
+    return state.set('digits', digits).set('pickingPlayerId', null).set('locked', true)
   }
-}
+}, initialState)
 
 export default grid
