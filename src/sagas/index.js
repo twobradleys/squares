4adebe81bd2e51230ad3a82817e307aea8d6526a
Bradley Buda <bradleybuda@gmail.com>
2016-12-29 11:01:24 -0800

    hook up manual refresh

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,14 +1,21 @@
-import { call, fork, put } from 'redux-saga/effects'
+import { call, fork, put, takeEvery } from 'redux-saga/effects'
 import { api } from '../services'
 import * as actions from '../actions'
 
 function* getAllTeams() {
   const teams = yield call(api.getTeams)
   yield put(actions.receiveTeams(teams))
+  // TODO error handling
+}
+
+function* watchGetAllTeams() {
+  yield takeEvery('FETCH_TEAMS', getAllTeams)
 }
 
 export default function* root() {
   yield [
-    fork(getAllTeams)
+    fork(getAllTeams), // fire this off once at initialization
+    fork(watchGetAllTeams)
+//    fork(watchCreateTeam)
   ]
 }
