cf1e2d03bbd66211b78c5c01495b35c663f802b4
Bradley Buda <bradleybuda@gmail.com>
2017-01-03 22:03:02 -0800

    Simplification

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,17 +2,25 @@ import React from 'react'
 
 import Cell from './Cell'
 import TeamDigits from '../containers/TeamDigits'
-import GameGrid from '../containers/GameGrid'
+import EntryCell from '../containers/EntryCell'
 
 const Grid = () => (
-  <div style={{display: 'flex', flexDirection: 'column'}}>
-    <div style={{display: 'flex', flexDirection: 'row'}}>
+  <div className="FlexColumn">
+    <div className="FlexRow">
       <Cell active={false} />
       <TeamDigits team="home" />
     </div>
-    <div style={{display: 'flex', flexDirection: 'row'}}>
+    <div className="FlexRow">
       <TeamDigits team="away" />
-      <GameGrid />
+      <div className="FlexColumn">
+        {[...Array(10)].map((_, i) => (
+           <div key={i} className="FlexRow">
+             {[...Array(10)].map((_, j) => (
+                <EntryCell key={j} i={i} j={j} />
+              ))}
+           </div>
+         ))}
+      </div>
     </div>
   </div>
 )
